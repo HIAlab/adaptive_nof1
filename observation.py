bacc3063278d103afafacd3a79d8b2a256d26969
Dominik Meier <dominik.meier@student.hpi.de>
2023-03-13 15:27:11 +0100

    Add toy example with linear regression and bayesian model

---
+++ observation.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import List
+import pandas as pd
+
+@dataclass
+class Observation:
+    c: List[float]
+    treatments: List[float]
+    y: List[float]
+
+    def linear_coefficients(self):
+        treatments = self.treatments
+        return pd.DataFrame({'T_1': (treatments==1)*1, 'T_2': (treatments==2)*1, 'c_1': self.c * (treatments==1), 'c_2': self.c * (treatments==2)})
+    
+    def dataFrame(self):
+        lc = self.linear_coefficients()
+        lc['y'] = self.y
+        return lc
+        
+    
---
+++ simulation.py
@@ -0,0 +1,45 @@
+from observation import Observation
+from treatmentplan import TreatmentPlan
+from dataclasses import dataclass, field
+from typing import List
+
+import numpy
+from numpy.random import default_rng
+
+@dataclass
+class Simulation:
+    mu_p: float
+    epsilon_i_sigma: float
+    c_sigma: float
+    treatment_plan: TreatmentPlan
+    mu_T: List[float]
+    alpha: List[float]
+    rng: numpy.random.Generator = field(init=False)
+    
+    def __post_init__(self):
+        RANDOM_SEED = 8927
+        numpy.random.seed(RANDOM_SEED)
+        self.rng = default_rng()
+    
+    def sample_observation(self) -> Observation:
+        n = self.treatment_plan.n_observations
+        treatments = self.treatment_plan.treatments()
+        
+        epsilon = self.rng.standard_normal(n) * self.epsilon_i_sigma
+        c = self.rng.standard_normal(n) * self.c_sigma
+        y = (
+            self.mu_p + epsilon
+            + ((treatments == 1) * (self.mu_T[0] + self.alpha[0] * c))
+            + ((treatments == 2) * (self.mu_T[1] + self.alpha[1] * c))
+        )
+        return Observation(**{
+            'c': c,
+            'treatments': self.treatment_plan.treatments(),
+            'y': y,
+        })
+    
+    def true_coefficients(self):
+        return {'mu_T': self.mu_T[0], 'mu_T_2': self.mu_T[1], 'alpha_T_1': self.alpha[0], 'alpha_T_2': self.alpha[1]}
+
+    def reference_values(self):
+        return {'mu_T': self.mu_T, 'mu_p':self.mu_p, 'alpha': self.alpha, 'epsilon_i_sigma': epsilon_i_sigma }
