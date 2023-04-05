82f211c893a109e8d802b45195abf902693af0d8
Dominik Meier <dominik.meier@student.hpi.de>
2023-04-05 17:20:56 +0200

    Implement constant policy

--- src/policy.py
+++ src/policy.py
@@ -1,5 +1,19 @@
-class Policy:
+from abc import ABC, abstractmethod
+
+
+class AbstractPolicy(ABC):
     def __init__(self, number_of_actions):
         self.number_of_actions = number_of_actions
 
-    def choose_action(history):
+    @abstractmethod
+    def choose_action(self, history):
+        pass
+
+
+class ConstantPolicy(AbstractPolicy):
+    def __init__(self, number_of_actions, action):
+        self.action = action
+        super.__init__(number_of_actions)
+
+    def choose_action(self, _):
+        return self.action
